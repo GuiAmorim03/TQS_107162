e3ace69d68f255014bdbc9f8e6f3329856a905af
GuiAmorim03 <guilhermeamorim@ua.pt>
2024-04-16 10:47:05 +0100

    lab3 spring init

---
+++ Labs/lab7/lab7_3/containers-dbmigration/src/main/java/tqs/ua/pt/containersdbmigration/ContainersDbmigrationApplication.java
@@ -0,0 +1,13 @@
+package tqs.ua.pt.containersdbmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainersDbmigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainersDbmigrationApplication.class, args);
+	}
+
+}
---
+++ Labs/lab7/lab7_3/containers-dbmigration/src/test/java/tqs/ua/pt/containersdbmigration/ContainersDbmigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.ua.pt.containersdbmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainersDbmigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
